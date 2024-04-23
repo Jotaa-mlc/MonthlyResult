db1651bc521064fcb7e980e475640fdd7822c643
Jotaa-mlc <joaoart.mlc23@gmail.com>
2024-04-23 11:16:42 -0300

    Classes para agrupar as colunas das planilhas

--- month_result.py
+++ month_result.py
@@ -22,10 +22,11 @@ class MonthResult:
             return None
         
         stock_ws = stock_wb.active
+        stock_columns = settings.stockCCV_columns()
         
         for product_row in range(1, stock_ws.max_row):
-            amount = stock_ws["C" + str(product_row)].value
-            cost = stock_ws["E" + str(product_row)].value
+            amount = stock_ws[stock_columns.amount + str(product_row)].value
+            cost = stock_ws[stock_columns.cost + str(product_row)].value
             self.stock_cost += amount * cost
         
         stock_wb.close()
@@ -37,39 +38,41 @@ class MonthResult:
             return None
         
         document_ws = document_wb.active
+        document_columns = settings.document_columns()
         
         for document_row in range(1, document_ws.max_row):
-            is_pedido = document_ws["A" + str(document_row)].value == "Pedido   Saída"
-            is_fechado = document_ws["C" + str(document_row)].value == "Fechado"
-            is_from_month = common.IsFromMonth(document_ws["I" + str(document_row)].value, self.month, self.year) if is_fechado else False
+            is_pedido = document_ws[document_columns.tp_doc + str(document_row)].value == "Pedido   Saída"
+            is_fechado = document_ws[document_columns.status_doc + str(document_row)].value == "Fechado"
+            is_from_month = common.IsFromMonth(document_ws[document_columns.dt_doc + str(document_row)].value, self.month, self.year) if is_fechado else False
             
             if is_pedido and is_fechado and is_from_month:
-                self.bills_plan.accounts[0].sub_accounts[0].value += document_ws["V" + str(document_row)].value #DINHEIRO
-                self.bills_plan.accounts[0].sub_accounts[1].value += document_ws["W" + str(document_row)].value #CHEQUE
-                self.bills_plan.accounts[0].sub_accounts[5].value += document_ws["X" + str(document_row)].value #TRANSFERENCIA / PIX
-                self.bills_plan.accounts[0].sub_accounts[2].value += document_ws["Y" + str(document_row)].value #CARTAO
-                self.bills_plan.accounts[0].sub_accounts[3].value += document_ws["Z" + str(document_row)].value #FATURADO
-                self.bills_plan.accounts[0].sub_accounts[4].value += document_ws["AA" + str(document_row)].value #FINANCEIRA
+                self.bills_plan.accounts[0].sub_accounts[0].value += document_ws[document_columns.pmt_dinheiro + str(document_row)].value #DINHEIRO
+                self.bills_plan.accounts[0].sub_accounts[1].value += document_ws[document_columns.pmt_cheque + str(document_row)].value #CHEQUE
+                self.bills_plan.accounts[0].sub_accounts[5].value += document_ws[document_columns.pmt_transferencia + str(document_row)].value #TRANSFERENCIA / PIX
+                self.bills_plan.accounts[0].sub_accounts[2].value += document_ws[document_columns.pmt_cartao + str(document_row)].value #CARTAO
+                self.bills_plan.accounts[0].sub_accounts[3].value += document_ws[document_columns.pmt_faturado + str(document_row)].value #FATURADO
+                self.bills_plan.accounts[0].sub_accounts[4].value += document_ws[document_columns.pmt_financeira + str(document_row)].value #FINANCEIRA
         
         self.bills_plan.accounts[0].CalculateAccount()
         document_wb.close()
     
     def CalcCMV(self) -> None:
         file_path = common.SubFilePath(self.month, self.year, False)
-        lucratividades_wb = common.LoadSheet(file_path)
+        profits_wb = common.LoadSheet(file_path)
         cmv = 0.0
         
-        if lucratividades_wb == None:
+        if profits_wb == None:
             self.bills_plan.accounts[2].sub_accounts[0].value = cmv
             return None
         
-        lucratividades_ws = lucratividades_wb.active
+        profits_ws = profits_wb.active
+        profits_columns = settings.profits_columns()
         
-        for product_row in range(1, lucratividades_ws.max_row):
-            cmv -= lucratividades_ws["E" + str(product_row)].value
+        for product_row in range(1, profits_ws.max_row):
+            cmv -= profits_ws[profits_columns.cost_prod + str(product_row)].value
         
         self.bills_plan.accounts[2].sub_accounts[0].value = cmv
-        lucratividades_wb.close()
+        profits_wb.close()
     
     def CalcDespesas(self) -> None:
         #CALCULANDO DESPESAS DA TABELA DE CONTAS A PAGAR
@@ -80,33 +83,35 @@ class MonthResult:
             return None
         
         payments_ws = payments_wb.active
+        payments_columns = settings.payment_columns()
         cashier_ws = cashier_wb.active
+        cahsier_columns = settings.cashier_columns()
         
         for payment_row in range(1, payments_ws.max_row):
-            is_quitado = payments_ws["H" + str(payment_row)].value == "Quitada"
-            is_2plano_contas = payments_ws["M" + str(payment_row)].value == "True"
-            is_from_month = common.IsFromMonth(payments_ws["P" + str(payment_row)].value, self.month, self.year) if is_quitado else False
+            is_quitado = payments_ws[payments_columns.status_pmt + str(payment_row)].value == "Quitada"
+            is_2plano_contas = payments_ws[payments_columns.is_2plano_contas + str(payment_row)].value == "True"
+            is_from_month = common.IsFromMonth(payments_ws[payments_columns.dt_pmt + str(payment_row)].value, self.month, self.year) if is_quitado else False
             
             if is_quitado and is_from_month and is_2plano_contas:
-                cod_conta = payments_ws["N" + str(payment_row)].value - 1
-                cod_subconta = payments_ws["O" + str(payment_row)].value - 1
+                cod_conta = payments_ws[payments_columns.cod_conta + str(payment_row)].value - 1
+                cod_sub_conta = payments_ws[payments_columns.cod_sub_conta + str(payment_row)].value - 1
                 
-                self.bills_plan.accounts[cod_conta].sub_accounts[cod_subconta].value -= payments_ws["S" + str(payment_row)].value
+                self.bills_plan.accounts[cod_conta].sub_accounts[cod_sub_conta].value -= payments_ws[payments_columns.pmt_value + str(payment_row)].value
         
         #CALCULANDO DESPESAS DA TABELA DE LANCAMENTOS LIVRO CAIXA
         for payment_row in range(1, cashier_ws.max_row):
-            is_2plano_contas = cashier_ws["H" + str(payment_row)].value == "True"
-            is_from_month = common.IsFromMonth(cashier_ws["C" + str(payment_row)].value, self.month, self.year) if is_2plano_contas else False
+            is_2plano_contas = cashier_ws[cahsier_columns.is_2plano_contas + str(payment_row)].value == "True"
+            is_from_month = common.IsFromMonth(cashier_ws[cahsier_columns.dt_pmt + str(payment_row)].value, self.month, self.year) if is_2plano_contas else False
             
             if is_2plano_contas and is_from_month:
-                is_saida = cashier_ws["D" + str(payment_row)].value == "Saída"
-                cod_conta = cashier_ws["I" + str(payment_row)].value - 1
-                cod_subconta = cashier_ws["J" + str(payment_row)].value - 1
+                is_saida = cashier_ws[cahsier_columns.tp_pmt + str(payment_row)].value == "Saída"
+                cod_conta = cashier_ws[cahsier_columns.cod_conta + str(payment_row)].value - 1
+                cod_sub_conta = cashier_ws[cahsier_columns.cod_sub_conta + str(payment_row)].value - 1
                 
-                payment_value = cashier_ws["G" + str(payment_row)].value
-                payment_value = -1 * payment_value if is_saida else payment_value
+                payment_value = cashier_ws[cahsier_columns.pmt_value + str(payment_row)].value
+                payment_value = payment_value if is_saida else (-1 * payment_value)
                                 
-                self.bills_plan.accounts[cod_conta].sub_accounts[cod_subconta].value -= cashier_ws["G" + str(payment_row)].value
+                self.bills_plan.accounts[cod_conta].sub_accounts[cod_sub_conta].value -= payment_value
         
         #CALCULANDO O CUSTO DA MERCADORIA VENDIDA (EXCLUNDO DUPLA CONTAGEM DE FRETE E ICMS)
         self.CalcCMV()
--- settings.py
+++ settings.py
@@ -13,3 +13,37 @@ def init():
     export_file = "Resultado Hidraucenter Nordeste.xlsx"
     global print_size
     print_size = 50
+class document_columns():
+    def __init__(self) -> None:
+        self.tp_doc = 'A'
+        self.status_doc = 'C'
+        self.dt_doc = 'I'
+        self.pmt_dinheiro = 'V'
+        self.pmt_cheque = 'W'
+        self.pmt_transferencia = 'X'
+        self.pmt_cartao = 'Y'
+        self.pmt_faturado = 'Z'
+        self.pmt_financeira = 'AA'
+class payment_columns():
+    def __init__(self) -> None:
+        self.status_pmt = 'H'
+        self.is_2plano_contas = 'M'
+        self.cod_conta = 'N'
+        self.cod_sub_conta = 'O'
+        self.dt_pmt = 'P'
+        self.pmt_value = 'S'
+class cashier_columns():
+    def __init__(self) -> None:
+        self.dt_pmt = 'C'
+        self.tp_pmt = 'D'
+        self.is_2plano_contas = 'H'
+        self.cod_conta = 'I'
+        self.cod_sub_conta = 'J'
+        self.pmt_value = 'G'
+class stockCCV_columns():
+    def __init__(self) -> None:
+        self.amount = 'C'
+        self.cost = 'E'
+class profits_columns():
+    def __init__(self) -> None:
+        self.cost_prod = 'E'
